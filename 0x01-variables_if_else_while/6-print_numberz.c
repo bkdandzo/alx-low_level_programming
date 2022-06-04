d6138ecdcfc798c8aae13793648f7824b9adb163
Bless Kojo Dandzo <blesskojodandzo17@gmail.com>
2022-06-03 20:02:45 -0700

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit number only using putchar
+ *  
+ * Return: Always 0
+ */
+int main(void)
+{
+		int digit;
+
+		for (digit = 0; digit <10; digit++)
+			putchar(digit + '0');
+
+		putchar("\n");
+
+		return (0);
+}
