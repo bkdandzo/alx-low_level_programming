49d42eadc465090d22365aaec3d8c2df253d3e63
Bless Kojo Dandzo <blesskojodandzo17@gmail.com>
2022-06-09 09:15:09 -0700

    Nine times task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,31 +6,32 @@
 
 void times_table(void)
 {
-	int n;
-	int m;
-	int mult;
+		int n;
+		int m;
+		int mult;
 
-	for (n = 0; n <= 9; n++)
-	{
-		for (m = 0; m <= 9; m++)
+		for (n = 0; n <= 9; n++)
 		{
-			mult = (m * n);
-			if (mult <= 9)
+			for (m = 0; m <= 9; m++)
 			{
-				_putchar(mult + 48);
+				mult = (m * n);
+				if (mult <= 9)
+				{
+					_putchar(mult + 48);
+				}
+				else
+				{
+					_putchar(mult / 10 + 48);
+					_putchar(mult % 10 + 48);
+				}
+				if (m < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if ((n * (m + 1)) <= 9 && m != 9)
+					_putchar(' ');
 			}
-			else
-			{
-				_putchar(mult / 10 + 48);
-				_putchar(mult % 10 + 48);
-			}
-			if (m < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			if ((n * (m + 1)) <= 9 && m != 9)
-				_putchar(' ');
+			_putchar('\n');
 		}
-		_putchar('\n');
-	}
+}
