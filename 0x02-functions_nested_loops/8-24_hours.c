dca29be683e24cd695bcd6a394547a5d87955eeb
Bless Kojo Dandzo <blesskojodandzo17@gmail.com>
2022-06-09 08:20:39 -0700

    24 hours task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,13 +9,15 @@ void jack_bauer(void)
 		int h;
 		int m;
 
-		for (h = 0; h <= 23; h++)
+		for (h =0; h <= 23; h++)
 		{
 			for (m = 0; m <= 59; m++)
-                        {
+			{
 				_putchar(h / 10 + 48);
-				_putchar(h % 10 + 4);
-				_putchar(':');
-				_putchar(m / 10 + 48);											           _putchar(m % 10 + 48);							
-				_putchar('\n');												   } 
+				_putchar(h % 10 + 48);
+				_putchar('-');
+				_putchar(m / 10 + 48);
+				_putchar(m % 10 + 48);
+				_putchar('\n');
+			}
 }
