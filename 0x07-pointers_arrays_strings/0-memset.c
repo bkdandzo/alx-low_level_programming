2513d72588decfcef2b70af6588f81c0be2e397d
Bless Kojo Dandzo <blesskojodandzo17@gmail.com>
2022-06-20 17:58:41 -0700

    Fiiling with c byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,19 @@
-# include "main.h"
+#include "main.h"
 /**
  * _memset - Function that fill memory with a constant byte
  * @s: string
  * @b: constant byte
- * @n: value of bytes of memory area
+ * @n: value of bytes of a memory area
  * Return: pointer to memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-		int i = 0;
-		while (n > 0)
-		{
-			s[i] = b;
-			i++;
-			n--;
-		}
-		return (s);
+	int = 0;
+	while (n > 0)
+	{
+		s[i] = b;
+		i++;
+		n--;
+	}
+	return (s);
 }
