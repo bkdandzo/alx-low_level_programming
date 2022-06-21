fccf3105adf7eb0f75e593f8d5a746ca1c16396f
Bless Kojo Dandzo <blesskojodandzo17@gmail.com>
2022-06-21 05:00:06 -0700

    Checks uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
- * @letter: the character to be tracked
- * Return: 1 if c is uppercase, 0 otherwise
+ * _isupper - check for uppercase character
+ * @c: character
+ * Return: 1 if c is uppercase otherwise 0
  */
-int _isupper(int letter)
+
+int _isupper(int c)
 {
-	return (letter >= 'A' && letter <= 'Z');
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
