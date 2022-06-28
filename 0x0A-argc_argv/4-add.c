3b6920d64ddf89c77ddd56ae5e122fd5e285fdfe
Bless Kojo Dandzo <blesskojodandzo17@gmail.com>
2022-06-28 04:49:31 -0700

    Adds two positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,42 +1,28 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 /**
- * main - prints the sum of positive a numbers
- * @argc: number of arguments
- * @argv[]: array of argc size
- * Return: Always 0 (success)
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-int i, j, len, sum = 0;
-char *ptr;
+	int i, j, add = 0;
 
-if (argc < 2)
-printf("0\n");
-else
-{
-for (i = 1; i < argc; i++)
-{
-ptr = argv[i];
-len = strlen(ptr);
-
-for (j = 0; j < len; j++)
-{
-if (isdigit(*(ptr + j)) == 0)
-{
-printf("Error\n");
-return (1);
-}
-}
-
-sum = sum + atoi(argv[i]);
-}
-
-printf("%d\n", sum);
-}
-
-return (0);
-}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
 }
