4fbd96f4a8f864e561377de379a458b77972c2ae
Bless Kojo Dandzo <blesskojodandzo17@gmail.com>
2022-06-28 04:19:59 -0700

    Prints number of arguments received

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,13 +8,13 @@
  */
 int main(int argc, char *argv[])
 {
-  int i = 0;
+	int i = 0;
 
-  while (i < argc)
-  {
-      printf("%s\n", argv[i]);
-      i++;
-  }
-  
-  return (0);
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+	return (0);
 }
