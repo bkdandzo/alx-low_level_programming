7e535d80fb1b4c91520da80e55143d5d23325a1e
Bless Kojo Dandzo <blesskojodandzo17@gmail.com>
2022-06-30 16:01:22 -0700

    Concatenates two string in malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,30 +1,71 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
- * _calloc - allocates memory for an array using malloc
- * @nmemb: number of elements
- * @size: byte sie of each array element
- * Return: pointer - t o newly allocated space in memory,
- *         or NULL - if nmemb = 0, size = 0 or function fails
+ * count_string - Get the size of a string.
+ *
+ * @string: base string.
+ *
+ * Return: pointer to a new space on memory, otherwise return NULL.
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+
+unsigned int count_string(char *string)
 {
-void *arr1;
-char *arr2;
-unsigned int i;
+	int i = 0;
 
-if (nmemb == 0 || size == 0)
-return (NULL);
+	while (string[i])
+		i++;
+	return (i);
+}
 
-arr1 = malloc(nmemb * size);
+/**
+ * string_nconcat - concatenates two strings.
+ *
+ * @s1: base string.
+ *
+ * @s2: complementary string.
+ *
+ * @n: number of characters to take of s2.
+ *
+ * Return: pointer to a new space on memory, otherwise return NULL.
+ */
 
-if (arr1 == NULL)
-return (NULL);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, size_of_s1 = 0, size_of_s2 = 0, size_of_final_string = 0;
+	int holder = n;
+	char *final_string = NULL;
 
-arr2 = arr1;
+	if (s1)
+		size_of_s1 = count_string(s1);
+	if (s2)
+		size_of_s2 = count_string(s2);
 
-for (i = 0; i < (nmemb * size); i++)
-arr2[i] = '\0';
+	if (n >= size_of_s2)
+		n = size_of_s2;
 
-return (arr1);
+	if (holder >= 0)
+		size_of_final_string = size_of_s1 + n + 1;
+	else
+		size_of_final_string = size_of_s1 + 1;
+
+	if (s1 || s2)
+		final_string = malloc(size_of_s1 + n + 1 * sizeof(char));
+	else if (!s1 && !s2)
+		final_string = malloc(1);
+
+	if (!final_string)
+	{
+		free(final_string);
+		return (NULL);
+	}
+
+	for (i = 0; i < (size_of_final_string - 1); i++)
+	{
+		if (i < size_of_s1)
+			final_string[i] = s1[i];
+		else if (i >= size_of_s1)
+			final_string[i] = s2[i - size_of_s1];
+	}
+	final_string[i] = '\0';
+	return (final_string);
 }
